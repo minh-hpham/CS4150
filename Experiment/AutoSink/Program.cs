fa71e74ebbc7b3245e99fee7a3ef2943a4e6894c
Minh Pham <phamhongminh@live.com>
2017-02-10 22:07:36 -0700

    run time error

--- Experiment/AutoSink/Program.cs
+++ Experiment/AutoSink/Program.cs
@@ -28,7 +28,7 @@ namespace AutoSink
     {
 
         public static int clock = 1;
-        public static List<Vertex> vertices = new List<Vertex>();
+        public static Dictionary<String, Vertex> vertices = new Dictionary<String, Vertex>();
         static void Main(string[] args)
         {
             // number of cities
@@ -38,7 +38,7 @@ namespace AutoSink
             {
                 string[] split = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.None);
                 // add new vertex with name is split[0] and toll is split[1]
-                vertices.Add(new Vertex(split[0], long.Parse(split[1])));
+                vertices.Add(split[0],new Vertex(split[0], long.Parse(split[1])));
             }
             // number of highway
             int h = Int32.Parse(Console.ReadLine());
@@ -48,22 +48,24 @@ namespace AutoSink
                 // each string is in format: "city1 city2". Unique
                 string[] split = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.None);
                 // add edge to vertex with name = split[0]
-                vertices.Find(v => v.name.Equals(split[0])).edges.AddLast(vertices.Find(v => v.name.Equals(split[1])));
+                vertices[split[0]].edges.AddLast(vertices[split[1]]);
 
             }
 
-            dfs(vertices.First());
+            dfs(vertices.First().Value);
 
-            var order = from vertex in vertices
-                          orderby vertex.post descending
-                          select vertex;
+            // descending order by post number
 
-
+            var items = from pair in vertices
+                        orderby pair.Value.post descending
+                        select pair;
+            //var order = vertices.Values.OrderByDescending(a => a.post).ToList();
+            var order = vertices.OrderByDescending(a => a.Value.post).ToList();
             // number of trips
             int t = Int32.Parse(Console.ReadLine());
 
-
             List<String> print = new List<string>();
+
             for (int i = 0; i < t; i++)
             {
                 // each string is in format: "city1 city2". Unique
@@ -71,32 +73,44 @@ namespace AutoSink
 
                 String start = split[0];
                 String end = split[1];
+                // if departure and destination is the same return 0 for toll
+                if (start.Equals(end)) print.Add(0.ToString());
 
-                int startIndex = vertices.FindIndex(vertex => vertex.name.Equals(start));
-                int endIndex = vertices.FindIndex(vertex => vertex.name.Equals(end));
-
-                if (startIndex == endIndex) print.Add(0.ToString());
-                else if (startIndex > endIndex) print.Add("NO");
                 else
                 {
-                    long toll = 0;
+                    int startIndex = order[start].;
+                    int endIndex = order.FindIndex(vertex => vertex.name.Equals(end));
 
-                    while (startIndex < endIndex)
+                    // can't find path because no vertex from start to end
+                    if (startIndex > endIndex) print.Add("NO");
+                    else
                     {
-                        if (vertices.ElementAt(startIndex).edges.Count == 0)
+                        var list = new Dictionary<Vertex, long>();
+
+                        list.Add(order.ElementAt(startIndex), 0);
+                        // a list contains vertex and total travel toll at that vertex 
+                        // similar to the pseudocode you send me
+                        for (int j = startIndex + 1; j <= endIndex; j++)
                         {
-                            toll = 0;
-                            break;
+                            // set every total toll to INF except departure point (0)
+                            list.Add(order.ElementAt(j), int.MaxValue);
                         }
-                        else
-                        {
-                            startIndex += 1;
-                            toll += vertices.ElementAt(startIndex).toll;
+                        // go through every vertex from departure to destination according to the 
+                        // post number 
+                        foreach (Vertex v in list.Keys.ToList())
+                        { 
+                            // go to each child vertex
+                            foreach (Vertex u in v.edges.ToList())
+                            {
+                                long newToll = list[v] + u.toll;
+                                list[u] = newToll < list[u] ? newToll : list[u];
+                            }
                         }
+                        long toll = list.Last().Value;
+                        if (toll > 0) print.Add(toll.ToString());
+                        else print.Add("NO");
 
                     }
-                    if (toll == 0) print.Add("NO");
-                    else print.Add(toll.ToString());
                 }
 
             }
@@ -104,9 +118,10 @@ namespace AutoSink
             foreach (String s in print)
                 Console.WriteLine(s);
             Console.Read();
-
+            
         }
-       
+
+
 
         // set prevalue for vertice
         private static void previsit(Vertex v)
@@ -122,11 +137,11 @@ namespace AutoSink
         // depth first search to find post value of each vertex
         private static void dfs(Vertex G)
         {
-            foreach (Vertex v in vertices)
+            foreach (Vertex v in vertices.Values)
             {
                 v.visited = false;
             }
-            foreach (Vertex v in vertices)
+            foreach (Vertex v in vertices.Values)
             {
                 if (v.visited == false)
                 {
@@ -134,7 +149,8 @@ namespace AutoSink
                 }
             }
 
-            vertices.OrderBy(i => i.post);
+
+
         }
 
         private static void explore(Vertex G, Vertex v)
@@ -144,7 +160,7 @@ namespace AutoSink
 
             foreach (Vertex u in v.edges)
             {
-                if (u.visited == false) // if(!visted[u]
+                if (u.visited == false) 
                     explore(G, u);
             }
             postvisit(v);
