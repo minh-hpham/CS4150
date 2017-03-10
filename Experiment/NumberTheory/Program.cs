44db83d21fc37aa8e88127fe18ef02eb6756e5c8
u0975798 <u0975798@UNPC-31>
2017-03-10 14:21:23 -0700

    still not work

--- Experiment/NumberTheory/Program.cs
+++ Experiment/NumberTheory/Program.cs
@@ -10,36 +10,42 @@ namespace NumberTheory
     {
         static void Main(string[] args)
         {
+            List<string> print = new List<string>();
             string line;
             while ((line = Console.ReadLine()) != null)
             {
-                string[] split = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.None);
+                string[] split = line.Split(new char[] { ' ' }, StringSplitOptions.None);
 
                 if (split[0].Equals("gcd"))
                 {
                     long n = long.Parse(split[1]);
                     long m = long.Parse(split[2]);
-                    Console.WriteLine(gcd(n, m));
+                   
+                   Console.WriteLine(gcd(n, m).ToString());
+
                 }
                 else if (split[0].Equals("exp"))
                 {
                     long n = long.Parse(split[1]);
                     long m = long.Parse(split[2]);
                     long N = long.Parse(split[3]);
-                    Console.WriteLine(exp(n, m, N));
+                    Console.WriteLine(exp(n, m, N).ToString());
+
                 }
                 else if (split[0].Equals("inverse"))
                 {
                     long n = long.Parse(split[1]);
                     long m = long.Parse(split[2]);
-                    Console.WriteLine(inverse(n, m));
+                    long inv = inverse(n, m);
+                    string s = inv != -1 ? inv.ToString() : "none";
+                    Console.WriteLine(s);
+
                 }
                 else if (split[0].Equals("isprime"))
                 {
                     long n = long.Parse(split[1]);
-                    if(isprime(n) == false)
-                        Console.WriteLine("no");
-                    else Console.WriteLine("yes");
+                    string s = isprime(n) == true ? "yes" : "no";
+                    Console.WriteLine(s);
 
                 }
                 else if (split[0].Equals("key"))
@@ -47,16 +53,60 @@ namespace NumberTheory
                     long n = long.Parse(split[1]);
                     long m = long.Parse(split[2]);
                     Console.WriteLine(key(n, m));
+
                 }
+   
+
             }
+            //foreach (string s in print)
+            //    Console.WriteLine(s);
             Console.Read();
         }
 
-        private static bool key(long p, long q)
+        private static HashSet<int> getPrime(long x)
         {
+            HashSet<int> list = new HashSet<int>();
+            if (x % 2 == 0)
+                list.Add(2);
+            long sqrt = (long)Math.Ceiling(Math.Sqrt(x));
+            for (int i = 3; i < sqrt; i += 2)
+            {
+                if (x % i == 0)
+                    list.Add(i);
+            }
+            return list;
+        }
+        private static string key(long p, long q)
+        {
+            StringBuilder sb = new StringBuilder();
             long N = p * q;
+            sb.Append(N);
+            sb.Append(" ");
             long phi = (p - 1) * (q - 1);
-            return false;
+            HashSet<int> primes = getPrime(phi);
+            int e = 1;
+            if (primes.Contains(2) == false)
+            {
+                if (gcd(2, phi) == 1) e = 2;
+            }
+
+            else
+            {
+                while (true)
+                {
+                    e += 2;
+                    if (primes.Contains(e) == false)
+                    {
+                        if (gcd(e, phi) == 1) break;
+                    }
+                }
+            }
+            sb.Append(e);
+            sb.Append(" ");
+            long P = inverse(e, phi);
+            sb.Append(P);
+
+            return sb.ToString();
         }
 
         private static bool isprime(long n)
@@ -80,13 +130,13 @@ namespace NumberTheory
                 return new long[3] { r[1], r[0] - (a / b) * r[1], r[2] };
             }
         }
-        private static string inverse(long a, long N)
+        private static long inverse(long a, long N)
         {
             long[] r = ee(a, N);
             if (r[2] == 1)
-                return (r[0] % N).ToString();
+                return r[0] % N;
             else
-                return "none";
+                return -1;
         }
 
         private static long exp(long n, long m, long N)
