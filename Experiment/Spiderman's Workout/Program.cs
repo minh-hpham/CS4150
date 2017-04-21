a052f3b50da20314c9872d7e0a5dd7ff9bd4c3d6
Minh Pham <phamhongminh@live.com>
2017-04-21 10:00:10 -0600

    unsolved spiderman

--- Experiment/Spiderman's Workout/Program.cs
+++ Experiment/Spiderman's Workout/Program.cs
@@ -8,20 +8,27 @@ namespace Spiderman_s_Workout
 {
     class Program
     {
+        //static public string[] updown;
         static void Main(string[] args)
         {
             int N = Int32.Parse(Console.ReadLine());
             for (int i = 0; i < N; i++)
             {
+                int total = 0;
                 int m = Int32.Parse(Console.ReadLine());
                 int[] line = new int[m];
                 string[] split = Console.ReadLine().Split(new char[] { ' ' }, StringSplitOptions.None);
                 for (int j = 0; j < m; j++)
                 {
                     line[j] = Int32.Parse(split[j]);
+                    total += line[j];
 
                 }
-                Console.WriteLine(minheight(line, 0, 0));
+                String[,] updown = new String[m,total+1];
+                //String updown = null;
+                int maxheight = minheight(line, 0, 0, ref updown);
+                Console.WriteLine(updown);
+               // Console.WriteLine(step(line, updown));
             }
             Console.Read();
         }
@@ -60,27 +67,95 @@ namespace Spiderman_s_Workout
 
             return cache[0];
         }
-        private static int minheight(int[] array, int steps, int height)
+        private static string step(int[] array, string[] updown)
+        {
+            StringBuilder sb = new StringBuilder();
+            int currentheight = 0;
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (updown[i].Equals("DOWN"))
+                {
+                    sb.Append('D');
+                    currentheight -= array[i];
+                }
+                else if (updown[i].Equals("UP"))
+                {
+                    sb.Append('U');
+                    currentheight += array[i];
+                }
+            }
+
+            if (currentheight != 0)
+                return "IMPOSSIBLE";
+            return sb.ToString();
+        }
+        //private static int minheight(int[] array, int steps, int height, ref String[] updown)
+        //{
+        //    int up, down;
+        //    if (steps == 0)
+        //    {
+        //        updown[steps] = "UP";
+        //        return minheight(array, steps + 1, array[steps], ref updown);
+        //    }
+        //    else if (steps == array.Length - 1)
+        //    {
+        //        updown[steps] = "DOWN";
+        //        return height;
+        //    }
+        //    else
+        //    {
+        //        up = minheight(array, steps + 1, height + array[steps], ref updown);
+        //        if (height - array[steps] >= 0)
+        //            down = minheight(array, steps + 1, height - array[steps], ref updown);
+        //        else
+        //            down = int.MaxValue;
+
+        //        int compare;
+        //        if (up <= down)
+        //        {
+        //            updown[steps] = "UP";
+        //            compare = up;
+        //        }
+        //        else
+        //        {
+        //            updown[steps] = "DOWN";
+        //            compare = down;
+        //        }
+        //        return Math.Max(height, compare);
+        //    }
+        //}
+        private static int minheight(int[] array, int steps, int height, ref string[,] updown)
         {
             int up, down;
+            
             if (steps == 0)
             {
-                return minheight(array, steps + 1, array[steps]);
+                updown[steps,array[steps]] = "UP";
+                return minheight(array, steps + 1, array[steps], ref updown);
             }
             else if (steps == array.Length - 1)
             {
+                updown[steps, array[steps]] = "DOWN";
                 return height;
             }
             else
             {
-                up = minheight(array, steps + 1, height + array[steps]);
                 if (height - array[steps] >= 0)
-                    down = minheight(array, steps + 1, height - array[steps]);
+                {
+                    up = minheight(array, steps + 1, height + array[steps], ref updown);
+                    down = minheight(array, steps + 1, height - array[steps], ref updown);
+                    if (up < down)
+                        updown[steps, array[steps]] = "UP";
+                    else updown[steps, array[steps]] = "DOWN";
+                    return Math.Max(height, Math.Min(up, down));
+                }   
                 else
-                    down = int.MaxValue;
-                return Math.Max(height, Math.Min(up, down));
+                {
+                    updown[index] = "UP";
+                    return Math.Max(height, minheight(array, steps + 1, height + array[steps], ref updown));
+                }
             }
         }
-
     }
 }
